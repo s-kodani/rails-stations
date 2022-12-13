5314518326d1a1d7275594aab9e139689bba76dc
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2022-12-13 23:54:05 +0900

    station6

--- app/helpers/admin/movies_helper.rb
+++ app/helpers/admin/movies_helper.rb
@@ -1,2 +1,11 @@
 module Admin::MoviesHelper
+  def is_showing_checked(is_showing_query_param, compare_str)
+    if is_showing_query_param == compare_str then
+      return true
+    elsif is_showing_query_param == nil && compare_str == "all" then
+      return true
+    else
+      return false
+    end
+  end
 end
