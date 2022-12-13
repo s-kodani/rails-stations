dfb74d8f9caf0936bd284116a29f76c7eb920679
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2022-12-14 00:04:02 +0900

    station6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @keyword = params[:keyword]
+    @is_showing = params[:is_showing]
+
+    if @keyword == nil && (@is_showing == nil || @is_showing == "all") then
+      @movies = Movie.all
+    elsif @keyword == nil then
+      @movies = Movie.where('is_showing = (?)', @is_showing)
+    elsif @is_showing == "all" || @is_showing == nil then
+      @like_keyword = '%'.concat(@keyword).concat('%')
+      @movies = Movie.where('name LIKE (?) OR description LIKE (?)', @like_keyword, @like_keyword)
+    else
+      @like_keyword = '%'.concat(@keyword).concat('%')
+      @movies = Movie.where('is_showing = (?) AND (name LIKE (?) OR description LIKE (?))', @is_showing, @keyword, @like_keyword)
+    end
   end
 end
--- app/helpers/admin/movies_helper.rb
+++ app/helpers/admin/movies_helper.rb
@@ -1,11 +1,2 @@
 module Admin::MoviesHelper
-  def is_showing_checked(is_showing_query_param, compare_str)
-    if is_showing_query_param == compare_str then
-      return true
-    elsif is_showing_query_param == nil && compare_str == "all" then
-      return true
-    else
-      return false
-    end
-  end
 end
